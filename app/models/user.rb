5033c1c0e107b68ff9750920abb7e65a6b4b04be
pmbrent <patriciambrent@gmail.com>
2015-11-17 09:36:00 -0500

    Create user model.

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ActiveRecord::Base
+
+  validates :name, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :librarian, inclusion: {in: [true, false]}
+  validates :password, length: {minimum: 8}, allow_nil: true
+
+  def self.find_by_credentials(userinfo, password)
+    user = User.find_by_email(userinfo) || User.find_by_name(userinfo)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def password
+    @password
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def isPassword(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+end
---
+++ db/migrate/20151117140000_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.boolean :librarian, default: false
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :name, unique: true
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
