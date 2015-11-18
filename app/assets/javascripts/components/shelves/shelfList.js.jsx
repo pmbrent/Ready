cbe53d056e374831d2ebf2cfcf32ef31402d277b
pmbrent <patriciambrent@gmail.com>
2015-11-18 17:53:57 -0500

    Restyle side column.

--- app/assets/javascripts/components/shelves/shelfList.js.jsx
+++ app/assets/javascripts/components/shelves/shelfList.js.jsx
@@ -3,7 +3,7 @@ window.ShelfList = React.createClass({
   render: function() {
     return (
       <div className="shelfList">
-        All Bookshelves:
+        <strong>All Bookshelves:</strong>
         {this.props.shelves.map(function(shelf) {
           return <p id={shelf.id}>{shelf.title} ({shelf.books.length})</p>;
         })}
