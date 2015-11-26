6821fe1e80e38d98a55541e98a772d386c315683
pmbrent <patriciambrent@gmail.com>
2015-11-26 09:44:47 -0500

    Include book data with Recommendations.

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,13 +1,21 @@
 class RecommendationsController < ApplicationController
 
   def show
-    @recommendations = Recommendation.where(user_id: params[:id])
+    @recommendations = reqs_with_books
     unless @recommendations
       Recommendation.generate_recs_for_user_id(params[:id])
-      @recommendations = Recommendation.where(user_id: params[:id])
+      @recommendations = reqs_with_books
     end
 
     render json: @recommendations
+  end
+
+  def reqs_with_books
+    Recommendation.joins(:book).where(recommendations: { user_id: 1 })
+      .select('recommendations.id, recommendations.book_id,
+              recommendations.rejected, recommendations.created_at,
+              books.title, books.isbn,
+              books.author, books.description')
   end
 
 end
